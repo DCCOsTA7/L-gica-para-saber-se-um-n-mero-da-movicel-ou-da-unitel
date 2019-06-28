e315a126a183c7558dc56e2b2d5c8a34bc57557f
DCCOsTA7 <40526855+DCCOsTA7@users.noreply.github.com>
2019-06-28 09:34:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerotelfn;
+            Console.WriteLine("Digite seu número de telefone");
+            numerotelfn = int.Parse(Console.ReadLine());
+            
+            if ((numerotelfn / 1000000 == 91) || (numerotelfn / 1000000 == 99))
+            {
+                Console.WriteLine("Seu número é da Movicel");
+
+            }
+            else 
+            {
+                Console.WriteLine("Seu número é da Unitel");
+                
+
+            }
+            
+                
+
+            
+            Console.ReadKey();
+        }
+
+    }
+}
